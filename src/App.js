4b86913390b6cf16fb3dd7195ab67e6cce775b6b
Arkadyuti Bandyopadhyay <bandyopadhyayarkadyuti@gmail.com>
2020-08-01 21:42:50 +0530

    Modifying starting component logic

--- src/App.js
+++ src/App.js
@@ -2,11 +2,21 @@ import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [count, setCount] = useState(0);
+  
+  const [color, setColor] = useState(null);
+  
+  useEffect(() => {
+    setColor(randomColor())
+  }, [count])
   
   return (
-    <div>
+    <div style={{ borderTop: `10px solid ${color}`}}>
+      {count}
+      <button onClick={() => setCount(currentCount => currentCount - 1)}>-</button>
+      <button onClick={() => setCount(currentCount => currentCount + 1)}>+</button>
     </div>
-  );
+  )
 }
 
 export default App;
